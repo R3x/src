b882e9137618e290e7c6670194b41dbb2ec0c59e
Kamil Rytarowski <n54@gmx.com>
2018-04-06 01:43:16 +0200

    Backport patches from pkgsrc for libtomcrypt

--- external/public-domain/libtomcrypt/dist/src/headers/tomcrypt_macros.h
+++ external/public-domain/libtomcrypt/dist/src/headers/tomcrypt_macros.h
@@ -242,7 +242,7 @@ asm __volatile__ (             \
 #define RORc(x,n) _lrotr(x,n)
 #define ROLc(x,n) _lrotl(x,n)
 
-#elif !defined(__STRICT_ANSI__) && defined(__GNUC__) && (defined(__i386__) || defined(__x86_64__)) && !defined(INTEL_CC) && !defined(LTC_NO_ASM)
+#elif !defined(__STRICT_ANSI__) && defined(__GNUC__) && (defined(__i386__) || defined(__x86_64__)) && !defined(INTEL_CC) && !defined(LTC_NO_ASM) && !defined(__clang__)
 
 static inline unsigned ROL(unsigned word, int i)
 {
@@ -341,7 +341,7 @@ static inline unsigned RORc(unsigned word, const int i)
 
 
 /* 64-bit Rotates */
-#if !defined(__STRICT_ANSI__) && defined(__GNUC__) && defined(__x86_64__) && !defined(LTC_NO_ASM)
+#if !defined(__STRICT_ANSI__) && defined(__GNUC__) && defined(__x86_64__) && !defined(LTC_NO_ASM) && !defined(__clang__)
 
 static inline unsigned long ROL64(unsigned long word, int i)
 {
